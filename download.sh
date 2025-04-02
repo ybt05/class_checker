51e3a5f92dc59f9d511f6451f7d4f63959201a2e
841 <q5078637@gmail.com>
2025-04-02 18:05:18 +0900

    　

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 # 年、zenki/kouki、01-06のリストを定義
 years=$(seq 2014 2024)
+years2=$(seq 2014 2023)
+years3=$(seq 2024 2024)
 terms=("zenki" "kouki")
 numbers=$(seq -w 01 06) # 01, 02, ..., 06
 
@@ -13,8 +15,17 @@ declare -A renameMap=(
     ["06,zenki"]="ID1.pdf" ["06,kouki"]="ID2.pdf"
 )
 
+declare -A renameMap2=(
+    ["01,zenki"]="common1.pdf" ["01,kouki"]="common2.pdf"
+    ["02,zenki"]="ID1.pdf" ["02,kouki"]="ID2.pdf"
+    ["03,zenki"]="IC1.pdf" ["03,kouki"]="IC2.pdf"
+    ["04,zenki"]="IS1.pdf" ["04,kouki"]="IS2.pdf"
+    ["05,zenki"]="IM1.pdf" ["05,kouki"]="IM2.pdf"
+    ["06,zenki"]="IN1.pdf" ["06,kouki"]="IN2.pdf"
+)
+
 # 各組み合わせに対してURLを生成し、ファイルをダウンロード
-for year in $years; do
+for year in $years2; do
     # 年度ごとのディレクトリを作成
     downloadPath="./static/pdf/$year"
     mkdir -p "$downloadPath"
@@ -34,13 +45,32 @@ for year in $years; do
     done
 done
 
-# 2014年からファイル名をリネーム
-for year in $years; do
+# 2014年から2023年までファイル名をリネーム
+for year in $years3; do
     for number in $numbers; do
         for term in "${terms[@]}"; do
             originalFileName="./static/pdf/$year/info-$year-$term-$number.pdf"
             if [[ -f "$originalFileName" ]]; then
                 newFileName="${renameMap["$number,$term"]}"
+                newFilePath="./static/pdf/$year/$newFileName"
+
+                # ファイル名をリネーム
+                mv "$originalFileName" "$newFilePath"
+                echo "Renamed: $originalFileName to $newFilePath"
+            else
+                echo "File not found: $originalFileName"
+            fi
+        done
+    done
+done
+
+# 2024年からファイル名をリネーム
+for year in $years; do
+    for number in $numbers; do
+        for term in "${terms[@]}"; do
+            originalFileName="./static/pdf/$year/info-$year-$term-$number.pdf"
+            if [[ -f "$originalFileName" ]]; then
+                newFileName="${renameMap2["$number,$term"]}"
                 newFilePath="./static/pdf/$year/$newFileName"
 
                 # ファイル名をリネーム
